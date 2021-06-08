000f506e37ac2f6a27511c701dc174b9d83493f3
DAMoreira <sk4rfac3@gmail.com>
2021-06-08 19:57:08 -0300

    fuentes

--- client/src/theme/typography.js
+++ client/src/theme/typography.js
@@ -1,5 +1,7 @@
+import '../fonts/Swiss721BT.ttf'
+
 export default {
   useNextVariants: true,
   fontSize: 11,
-  fontFamily: ['Montserrat', 'sans-serif', 'Helvetica Neue', 'Arial'].join(',')
+  fontFamily: ['Swiss 721 BT', 'sans-serif', 'Helvetica Neue', 'Arial'].join(','),
 };
